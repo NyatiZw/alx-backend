51d53cfee8b0992b6ebd9534c717374ffd55f1c3
NyatiZw <appsmaid19@gmail.com>
2023-10-28 12:31:56 +0000

    Basic dictionary

---
+++ 0x01-caching/0-basic_caching.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+"""This is the BasicCache module"""
+
+
+from base_caching import BaseCaching
+
+class BasicCache(BaseCaching):
+    """A caching system that doesn't have a limit"""
+
+    def put(self, key, item):
+        """
+        Store an item in the cache
+
+        Args:
+            key: The key for the item
+            item: The item to be stored
+
+        if key or item is None, this module does nothing
+        """
+        if key is not None and item is not None:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """
+        Retrieve an item from the cache.
+
+        Args:
+            key: The key for the item to be retrieved.
+
+        Returns:
+            The item linked to the given key, or None
+        """
+        if key is not None:
+            return self.cache_data.get(key)
+        return None
