eff0a1d901200ce62960cbcff68be80d55ca72c6
NyatiZw <appsmaid19@gmail.com>
2023-11-13 08:49:20 +0000

    hotfix

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -4,16 +4,22 @@
 
 from flask import Flask, render_template
 
-app = Flask(__name__)
+app: Flask = Flask(__name__)
 
 
 @app.route('/')
-def index():
+def index() -> str:
+    """
+    Render the index page.
+
+    Returns:
+        str: Rendered HTML content.
+    """
     return render_template(
-            'index.html',
-            title='Welcome to Holberton',
-            header='Hello world'
-        )
+        '0-index.html',
+        title='Welcome to Holberton',
+        header='Hello world'
+    )
 
 
 if __name__ == '__main__':
