6001500523ae31d2273ba6af23e23603592a7c73
NyatiZw <appsmaid19@gmail.com>
2023-11-13 09:02:56 +0000

    Basic babel set up

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+""" Basic Babel setup """
+
+
+from flask import Flask, render_template
+from flask_babel import Babel
+
+app: Flask = Flask(__name__)
+babel: Babel = Babel(app)
+
+
+class Config:
+    LANGUAGES: list[str] = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE: str = 'en'
+    BABEL_DEFAULT_TIMEZONE: str = 'UTC'
+
+
+app.config.from_object(Config)
+
+
+@app.route('/')
+def index() -> str:
+    return render_template(
+        '1-index.html',
+        title='Welcome to Holberton',
+        header='Hello world'
+    )
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
